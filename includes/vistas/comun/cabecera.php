6ac654510ac4e5354ffd6f8365df4050ef93fb98
danycasperngjay <dany.casperngjay@gmail.com>
2023-03-10 13:20:18 +0100

    Update cabecera.php

--- includes/vistas/comun/cabecera.php
+++ includes/vistas/comun/cabecera.php
@@ -1,5 +1,16 @@
 <?php
-	echo '<header>';
-        <div container
-	echo '</header>';
+	echo '<header style="background-color: #FBC485; height: 80px; width: 100%; text-align: center;">
+                <a href="inicio.com">
+                    <img src="../../../resources/logo.png"  alt="Logo" 
+                                                            height="80px" 
+                                                            style="float: left; margin-left: 15px;">
+                </a>
+
+                <img src="../../../resources/nombre.png"    alt="Nombre" 
+                                                            height="80px">
+
+                <img src="../../../resources/menu.png"  alt="Menu" 
+                                                        height="50px" 
+                                                        style="float: right; margin-right: 15px; margin-top:15px;">
+            </header>';
 ?>
