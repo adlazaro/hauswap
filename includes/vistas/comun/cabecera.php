4d27b0ac61eca277c45d37f2d11773c3f535c74e
Inés <iness.palero@gmail.com>
2023-03-12 20:23:56 +0100

    login y registro churro (sin conexión a base de datos ni nada)

--- includes/vistas/comun/cabecera.php
+++ includes/vistas/comun/cabecera.php
@@ -1,15 +1,15 @@
 <?php
 	echo '<header style="background-color: #FBC485; height: 80px; width: 100%; text-align: center;">
-                <a href="../../../index.php">
-                    <img src="../../../resources/logo.png"  alt="Logo" 
+                <a href="./index.php">
+                    <img src="./resources/logo.png"  alt="Logo" 
                                                             height="80px" 
                                                             style="float: left; margin-left: 15px;">
                 </a>
 
-                <img src="../../../resources/nombre.png"    alt="Nombre" 
+                <img src="./resources/nombre.png"    alt="Nombre" 
                                                             height="80px">
 
-                <img src="../../../resources/menu.png"  alt="Menu" 
+                <img src="./resources/menu.png"  alt="Menu" 
                                                         height="50px" 
                                                         style="float: right; margin-right: 15px; margin-top:15px;">
             </header>';
