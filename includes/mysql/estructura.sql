ae5a5a6ff140dc0f0d6c06b7dfb3d887eae74237
ayayahyaamar <ayayahya@ymail.com>
2023-03-13 12:00:06 +0100

    mas tablas

--- includes/mysql/estructura.sql
+++ includes/mysql/estructura.sql
@@ -3,7 +3,9 @@
 */
 DROP TABLE IF EXISTS `Reservas`;
 DROP TABLE IF EXISTS `Chat`;
-
+DROP TABLE IF EXISTS 'Roles';
+DROP TABLE IF NOT EXISTS `RolesUsuario`;
+DROP TABLE IF NOT EXISTS 'Usuarios';
 
 CREATE TABLE IF NOT EXISTS `Reservas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -11,7 +13,7 @@ CREATE TABLE IF NOT EXISTS `Reservas` (
   `casa_2` int(11) NOT NULL AUTO_INCREMENT,
   `fecha_entrada` DATETIME NOT NULL,
   `fecha_salida` DATETIME NOT NULL,
-  'estado' COLLATE utf8mb4_general_ci NOT NULL UNIQUE,
+  'estado' varchar(30) COLLATE utf8mb4_general_ci NOT NULL UNIQUE,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -23,3 +25,15 @@ CREATE TABLE IF NOT EXISTS `Chat` (
   'numero_mensajes' int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `Roles` (
+  `rol` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(30) COLLATE utf8mb4_general_ci NOT NULL UNIQUE,
+  PRIMARY KEY (`rol`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `RolesUsuario` (
+  `rol` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` int(11) NOT NULL AUTO_INCREMENT,
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
