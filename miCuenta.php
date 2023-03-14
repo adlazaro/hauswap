4fdd2fddc642c9f34d38badf30d9bc611d5b7107
Laura San Martín <72148756+laurasanmartind@users.noreply.github.com>
2023-03-14 10:14:22 +0100

    boton publicar en mi cuenta

---
+++ miCuenta.php
@@ -0,0 +1,9 @@
+<?php
+//require_once 'includes/config.php';
+
+$tituloPagina = 'Publicar';
+$contenidoPrincipal=<<<EOS
+<a href="publicarCasa.php"><button type="button">Nueva propiedad</button></a>
+EOS;
+require ('./includes/vistas/plantillas/plantilla.php');
+?>
