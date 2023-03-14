928f7cad1b92f25ca3195f9582ac78c2581e1864
danycasperngjay <dany.casperngjay@gmail.com>
2023-03-14 11:23:31 +0100

    esto mal

--- includes/vistas/comun/cabecera.php
+++ includes/vistas/comun/cabecera.php
@@ -1,14 +1,54 @@
 <?php
 		$mostrarMenu = false;
-
-		if (isset($_POST['mostrarOcultar'])) {
-			$mostrarMenu = ! $mostrarMenu; 
-		}
+        var_dump($_POST);
+        if (isset($_POST['mostrarOcultar'])) {
+            if ($mostrarMenu) {
+                $mostrarMenu = false;
+                var_dump($_POST);
+            } else {
+                $mostrarMenu = true;
+                var_dump($_POST);
+            }
+        }  
 ?>
 
 <?php
-	if ($mostrarMenu)
-        require('includes/vistas/comun/menu.php');
+// Obtener el valor actual de $mostrarMenu de la cookie, si existe
+if (isset($_COOKIE['mostrarMenu'])) {
+    $mostrarMenu = $_COOKIE['mostrarMenu'] == 'true';
+} else {
+    $mostrarMenu = false;
+}
+
+// Actualizar el valor de $mostrarMenu si se envió el formulario
+if (isset($_POST['mostrarOcultar'])) {
+    $mostrarMenu = !$mostrarMenu;
+    setcookie('mostrarMenu', $mostrarMenu ? 'true' : 'false', time() + 3600, '/');
+}
+	if ($mostrarMenu){
+        echo '<div style="background-color: #FBC485;    height: 100%; 
+        width: 25%;
+        position: fixed;
+        top: 0;
+        right: 0;
+        padding: 30px;">
+<a href="../../../index.php">
+Inicio
+</a> <br> <br>
+<a href="../../../chat.php">
+Chats
+</a> <br> <br>
+<a href="../../../miCuenta.php">
+Cuenta
+</a> <br> <br>
+<a href="../../../index.php">
+Cerrar Sesión
+</a> <br> <br>
+
+</div>';
+
+
+    }
 ?>
 
 <?php
@@ -24,11 +64,16 @@
                             
                 <!--Botón--!>
                 <form method="post">
-                <input type="image" name="mostrarOcultar"   src="./resources/menu.png"
-                                                            alt="Menu" 
-                                                            height="50px" 
-                                                            style="float: right; margin-right: 15px; margin-top:15px;">
+                <button type="submit" name="mostrarOcultar" style="background-image: src= "resources/menu.png"; 
+                                                            width: 500px; 
+                                                            height: 500px;
+                                                            float: right; 
+                                                            margin-rught: 15px;">
+                </button>
                 </form>
+
+
+                
 
                 <!--
                 <a href="./includes/vistas/comun/menu.php">
